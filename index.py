5b04a8ea2811a9d7c5feebbf2748e846ccb68daf
Leospard <694963063@qq.com>
2023-02-13 18:21:12 +0800

    add memory store alternative

--- index.py
+++ index.py
@@ -8,19 +8,23 @@ import urllib.parse
 # 获取随机字符串
 randomStr = lambda len: "".join(random.sample('abcdefghijklqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' * 100, len))
 
-AccessKey = {
-    "id": os.environ.get("ALI_KEY_ID"),
-    "secret": os.environ.get("ALI_KEY_SECRET"),
-}
-OSSConf = {
-    'endPoint': 'oss-' + os.environ.get("ALI_OSS_REGION") + '.aliyuncs.com',
-    # 'endPoint': 'oss-cn-hongkong.aliyuncs.com',
-    'bucketName': os.environ.get("BUCKET"),
-}
+def getBucket():
+    AccessKey = {
+        "id": os.environ.get("ALI_KEY_ID"),
+        "secret": os.environ.get("ALI_KEY_SECRET"),
+    }
+    OSSConf = {
+        'endPoint': 'oss-' + os.environ.get("ALI_OSS_REGION") + '.aliyuncs.com',
+        # 'endPoint': 'oss-cn-hongkong.aliyuncs.com',
+        'bucketName': os.environ.get("BUCKET"),
+    }
 
-# 获取获取/上传文件到OSS的临时地址
-auth = oss2.Auth(AccessKey['id'], AccessKey['secret'])
-bucket = oss2.Bucket(auth, OSSConf['endPoint'], OSSConf['bucketName'])
+    # 获取获取/上传文件到OSS的临时地址
+    auth = oss2.Auth(AccessKey['id'], AccessKey['secret'])
+    bucket = oss2.Bucket(auth, OSSConf['endPoint'], OSSConf['bucketName'])
+    return bucket
+
+url_map = {}
 
 # 首页
 @bottle.route('/')
@@ -34,8 +38,11 @@ def to_target(share):
     print(">>>>>>>>>>> to_target")
     random_file = '/tmp/%s' % (randomStr(5))
     try:
+        bucket = getBucket()
         bucket.get_object_to_file(share, random_file)
     except Exception as e:
+        if share in url_map :
+            bottle.redirect(url_map[share])
         print(e)
         return "短网址跳转失败，请确定短网址的正确性"
     with open(random_file) as f:
@@ -49,15 +56,22 @@ def to_target(share):
 @bottle.route('/api', method=['GET', 'POST'])
 def short_api():
     if bottle.request.method == "POST":
-        try:
-            body = json.loads(bottle.request.body.read().decode("utf-8"))
-            target = body.get("target")
-            source = randomStr(5)
-            bucket.put_object(source, target)
-            return {"status": 1, "url": '/t/' + source}
-        except Exception as e:
-            print(e)
-            return {"status": 0, "msg": '添加失败，异常未知'}
+        body = json.loads(bottle.request.body.read().decode("utf-8"))
+        target = body.get("target")
+        source = randomStr(5)
+        if os.environ.get("ALI_KEY_ID") != None and os.environ.get("ALI_KEY_SECRET") != None and os.environ.get("ALI_OSS_REGION") != None and os.environ.get("BUCKET"):
+            try:
+                bucket = getBucket()
+                bucket.put_object(source, target)
+                return {"status": 1, "url": '/t/' + source}
+            except Exception as e:
+                print(e)
+                url_map[source] = target
+                return {"status": 2, "url": '/t/' + source}
+        else:
+            url_map[source] = target
+            return {"status": 3, "url": '/t/' + source}
+        
 
 if __name__ == "__main__":
     bottle.run(host='0.0.0.0', debug=False, port=9000)
